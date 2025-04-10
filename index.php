f1d9a499437311de7d895e4cba1c55e8e2c815bf
Luzemário <luzemario@luzemario.net.br>
2025-04-09 21:41:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
+$servername = "localhost";
 $username = "root";
 $password = "Senha123";
 $database = "meubanco";
@@ -34,13 +34,13 @@ $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
 
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host, Telefone) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name', '$valor_rand2')";
 
 
 if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
+  echo "Novo registro criado com sucesso";
 } else {
-  echo "Error: " . $link->error;
+  echo "Erro: " . $link->error;
 }
 
 ?>
